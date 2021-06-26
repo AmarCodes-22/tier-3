1ceef5e014828bbdedcb79f7b8f6846d218ce550
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-06-26 15:08:40 +0530

    Added resizing in canny_edge_detector.py

--- edge detection/canny_edge_detector.py
+++ edge detection/canny_edge_detector.py
@@ -2,15 +2,18 @@ import cv2 as cv
 import os
 import numpy as np
 
-# Reads the image from the path specified.
-img = cv.imread(os.path.join(os.getcwd(), 'images', 'cube.jpg'))
+#* Setting up directories
+photos_loc = os.path.join(os.getcwd(), 'images')
 
-# Shows the image in a new window.
-cv.imshow('Cube', img)
+img = cv.imread(os.path.join(photos_loc, 'cube.jpg')) 
+
+#resized = cv.resize(img, (img.shape[1]//4, img.shape[0]//4))
+
+cv.imshow('Original', img)
 
 # Canny edge detector works great.
 canny = cv.Canny(img, 125, 175)
-cv.imshow('Edges detected in the image', canny)
+cv.imshow('Edges', canny)
 
 # Keeps the window(s) active indefinitely until a key is pressed.
 cv.waitKey(0)
